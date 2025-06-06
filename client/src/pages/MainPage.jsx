228924f97e91da7f5ce9c4a4cdb5a543fdeb38ab
akshav-coder <akshavanthvm@gmail.com>
2025-06-06 15:13:46 +0530

    removed unwanted files

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,15 +2,11 @@ import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 import HowItWorks from "../components/common/HowItWorks";
 import OnwardVisasFeatures from "../components/common/OnwardVisasFeatures";
-import ProjectTitle from "../components/common/ProjectTitle";
 import TestimonialSlider from "../components/common/TestimonialSlider";
 import TrustBadge from "../components/common/TrustBadge";
 import VisaHelpCards from "../components/common/VisaHelpCards";
-import flight_cloud from "../assets/flight_cloud.jpg";
 import "../styles/MainPage.css";
 import FormPage from "../components/common/FormPage";
-import SampleUI from "../components/common/SampleUI";
-import HotelSearch from "../components/common/HotelSearch";
 import PricingSection from "../components/common/PricingSection";
 
 const MainPage = () => {
